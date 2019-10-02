7d0f19a7419c4718e2d64e87e5f67c27f1aee4f1
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-02 14:29:11 -0600

    started work

--- Article/Article.js
+++ Article/Article.js
@@ -89,7 +89,7 @@ const data = [
 ];
 
 /* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
-  
+
   <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
@@ -112,3 +112,7 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+function (){
+
+};
